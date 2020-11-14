e725f8a08aa5b327d570a8719f8067fe0ffdeaa6
Marcel Heinz <marcel.heinz@gmail.com>
2020-11-14 15:28:07 +0100

    functional test changed

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,33 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http:/localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #check website
+        self.browser.get('http://localhost:8000')
+
+        #notice webiste 'SkinMate'
+        self.assertIn('SkinMate',self.browser.title)
+
+        self.fail('Finish the test')
+
+        #notice field to enter city name
+
+        #type "Schwerin" into text box
+
+        #hit enter, the page updates and shows the current uv index
+
+        #generate unique url to rember city
+
+        #re-vist site and uv index is still there for "Schwerin"
+
+        #end
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
